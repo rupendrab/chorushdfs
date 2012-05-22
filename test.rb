25aa90e7ac247438ddb97070927f1686dece2807
Vinicius Fuentes & Yaneeka Huq <pair+vinicius+yaneeka@pivotallabs.com>
2012-05-22 12:06:35 -0700

    Expanding test ruby code to cover file listing

--- test.rb
+++ test.rb
@@ -8,6 +8,19 @@ uri = URI.parse("http://localhost:5000/version?host=#{ENV["HOST"]}&port=8020&use
 
 # Shortcut
 response = Net::HTTP.get_response(uri)
-puts response.body
 
-exit response.body.empty? ? 1 : 0
+if !response.body.empty?
+    puts "Version OK: #{response.body}" 
+else
+    puts "Version NOT OK"
+end
+
+uri = URI.parse("http://localhost:5000/0.20.1gp/list/%2Fwebui%2F?host=gillette&port=8020&username=pivotal")
+response = Net::HTTP.get_response(uri)
+entries = JSON.parse(response.body)
+
+if entries.size > 0
+    puts "Listing OK: #{entries.size} items"
+else
+    puts "Listing NOT OK"
+end
