f77ff45098b7228147fa6f805c7cef7698a56c73
Brandon Liu & Vinicius Fuentes <pair+brandon+vinicius@pivotallabs.com>
2012-08-01 11:26:48 -0700

    Making TestConnect connect to all currently available versions of hadoop [#30298423]

--- hdfs-query-service/src/TestConnect.java
+++ hdfs-query-service/src/TestConnect.java
@@ -10,8 +10,35 @@ import com.emc.greenplum.hadoop.plugins.HdfsEntity;
  * To change this template use File | Settings | File Templates.
  */
 public class TestConnect {
-    public static void main(String [] args) {
-        Hdfs hdfs = new Hdfs("chorus-gphd02.sf.pivotallabs.com", "8020", "pivotal");
-        System.out.println(hdfs.getServerVersion().getName());
+    public static void main(String[] args) {
+        try {
+            HdfsVersion version = Hdfs.getServerVersion("chorus-gpmr12.sf.pivotallabs.com", "7222", "root");
+            System.out.println(version.getName());
+            Hdfs hdfs = new Hdfs("chorus-gpmr12.sf.pivotallabs.com", "7222", "root", version);
+            hdfs.list("/");
+        } catch (Exception e) {
+            System.out.println("Failed to connect to MapR!!!");
+            e.printStackTrace();
+        }
+
+        try {
+            HdfsVersion version = Hdfs.getServerVersion("chorus-gphd11.sf.pivotallabs.com", "8020", "root");
+            System.out.println(version.getName());
+            Hdfs hdfs = new Hdfs("chorus-gphd11.sf.pivotallabs.com", "8020", "root", version);
+            hdfs.list("/");
+        } catch (Exception e) {
+            System.out.println("Failed to connect to GPHD 1.1!!!");
+            e.printStackTrace();
+        }
+
+        try {
+            HdfsVersion version = Hdfs.getServerVersion("chorus-gphd02.sf.pivotallabs.com", "8020", "root");
+            System.out.println(version.getName());
+            Hdfs hdfs = new Hdfs("chorus-gphd02.sf.pivotallabs.com", "8020", "root", version);
+            hdfs.list("/");
+        } catch (Exception e) {
+            System.out.println("Failed to connect to GPHD 0.2!!!");
+            e.printStackTrace();
+        }
     }
 }
